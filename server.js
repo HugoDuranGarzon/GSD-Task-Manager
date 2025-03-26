40f588ab87b2bb153c50a07b3b3614ec5fdfa354
Hugo Durán Garzón <hugodurangarzon@gmail.com>
2025-03-27 00:18:18 +0100

    Task manager REST API implemented with Express js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+//Read JSON on request
+app.use(express.json());
+
 //Main route definition
 app.get('/', (req, res) => {
     res.send('Hello world from express!');
@@ -35,4 +38,81 @@ app.get('/evenorodd/:num', (req,res) => {
     const isEven = num % 2 === 0;
 
     res.json({number: num, isTheNumberEven: isEven});
-})
+});
+
+//Task manager REST API
+
+
+// Task object array 
+let tasks = [
+    {id: 0, title: 'default test task', completed: false},
+    {id: 1, title: 'make bed', completed: true}
+];
+
+
+
+//Get the tasks
+app.get('/tasks/:id/', (req, res) => {
+
+    const taskid = parseInt(req.params.id);
+
+    const task = tasks.find(t => t.id === taskid);
+
+    if(!task){
+        return res.status(404).json({error: "Task not found"});
+    }
+
+    res.json(task);
+    
+
+});
+
+//Update tasks [Updating using postman]
+
+app.put('/tasks/:id', (req, res) => {
+    const taskid = parseInt(req.params.id); 
+    const {title, completed} = req.body; // Get the data from the body
+
+    const task = tasks.find( t => t.id === taskid);
+
+    if(!task) {
+        return res.status(404).json({error: "Task not found"});
+    }
+
+    if (title) task.title = title;
+    if (completed) task.completed = completed;
+
+    res.json(task);
+});
+
+
+//Create tasks [Create using postman]
+app.post('/tasks/:id', (req, res) => {
+
+    const taskid = parseInt(req.params.id);
+    const {title, completed} = req.body;
+
+    tasks.push({"id": taskid, "title": title, "completed": completed});
+
+    const task = tasks.find(t => t.id === taskid);
+
+    res.json(task);
+
+
+});
+
+//Delete tasks [Delete using postman]
+app.delete('/tasks/:id', (req, res) => {
+
+    const taskid = parseInt(req.params.id);
+
+    const task = tasks.find(t => t.id === taskid);
+
+    const index = tasks.findIndex(t => t.id === taskid);
+
+    tasks.splice(index, 1);
+
+    res.json({"message": "Task deleted successfully", "task": task});
+
+
+});
